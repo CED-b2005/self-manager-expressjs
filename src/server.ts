380fd5b81a3396383a39b94c935ac37f18e0d0ec
CED-b2005 <duqingping2k5@gmail.com>
2025-12-29 11:38:14 +0700

    modify: [users-module] modify and set comment in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,33 @@
-import express from "express";
-import { Request, Response } from "express";
+import "reflect-metadata"; // reading class-validation
+import express from "express"; // using express
+import helmet from "helmet"; // protect header
+import cors from "cors"; // allow accept from other host
+import compression from "compression"; // send reponse faster
+import dotenv from "dotenv"; // select key from .env
+import { Request, Response } from "express"; // sort time will delete
 
+//app config
+dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(express.json()); // reading request body
+app.use(express.urlencoded({ extended: true })); // reading request body 
 
-app.get("/", (req:Request, res: Response)=> {
-    res.send("Hello Expreess");
-})
+// route - call api
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Expreess");
+});
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}\nlocalhost: http://localhost:3000`)
-})
+// export express - runing app
+const port = process.env.PORT || 3001;
+app.listen(port , () => {
+  console.log(`app listening on port ${port}\nlocalhost: http://localhost:${port}`);
+});
